119c76274e5fc3b72b63d52d0296992f205aa3cc
Uener Coelho <uenercoelho2@hotmail.com>
2022-11-24 20:42:48 -0400

    03-Arrays

---
+++ 01-Logica/03-Arrays.js
@@ -0,0 +1,12 @@
+/* Arrays
+  -> é uma variável onde conseguimos atribuir valores.
+*/
+
+var nomes = ['Uener', 'Catia', 'David', 'Leonardo'];
+
+console.log(nomes[0]+' é o pai do '+nomes[2]+' e do '+nomes[3]+', e a '+nomes[1]+' é a mãe.')
+
+console.log(`
+-> Agora com Template String:
+=> ${nomes[0]} é o pai do ${nomes[2]} e do ${nomes[3]}, e a ${nomes[1]} é a mãe.`)
+
