35b19cce0c8ad1732cdd58d21f359239483961c6
Uener Coelho <uenercoelho2@hotmail.com>
2022-11-30 22:10:29 -0400

    Leitura no Prompt

---
+++ 01-Logica/09-LeituraPrompt.js
@@ -0,0 +1,5 @@
+var nome = prompt('Qual o seu Nome?');
+var idade = prompt('Qual sua Idade?');
+var cidade = prompt('Qual Cidade você mora?');
+
+console.log(alert(`O nome é ${nome}, a idade é ${idade} e a cidade é ${cidade}.`))
