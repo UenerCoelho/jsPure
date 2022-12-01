093f39638e1d4e407cd932d308132e9635ae55d7
Uener Coelho <uenercoelho2@hotmail.com>
2022-11-30 20:53:08 -0400

    Laço de Repetição

---
+++ 01-Logica/07-LacosRepeticao.js
@@ -0,0 +1,15 @@
+var n = 0;
+while(n < 10){
+  console.log('0', n);
+  n++;
+}
+
+for(var i = 0; i < 10; i++) {
+  console.log('1', i);
+}
+
+do{
+  var d = 0;
+  console.log('2', d);
+  d++;
+}while (d < 10);
